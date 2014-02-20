020fb7cf0269b7908411c25d15943c17055fdc60
PC <PC@Yaakoubi-Sinda>
2014-02-20 15:55:25 +0100

    new commit by sinda

--- src/Test/Acceuil.java
+++ src/Test/Acceuil.java
@@ -602,6 +602,7 @@ public class Acceuil extends javax.swing.JFrame {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Acceuil().setVisible(true);
+                System.out.println();
             }
         });
     }
